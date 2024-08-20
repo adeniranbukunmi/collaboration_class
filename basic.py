3fdb84f9c1361dc9e9ef15e6dcad965041d86f1b
adeniran recab bukunmi <oluwabukunmiade1@gmail.com>
2024-08-20 09:31:02 +0100

    created an object car

--- basic.py
+++ basic.py
@@ -3,3 +3,27 @@ print("welcome to collaboration class")
 def square(x):
     return x **5
 print(square(4))
+
+"""
+working on object car
+types
+method: details(bukunmi)
+method: drive(pelumi)
+method: drift(boluwatife)
+method: park(ayodele)
+method: reverse(toluwanimi)
+method: ignition(maimunat)
+"""
+class Cars():
+    tyre=4
+    engine=1
+    bonnet=1
+    brake=1
+    accelerator=1
+    def __init__(self, color, make, model,year, price, engin_type):
+        self.color=color
+        self.maker=make
+        self.model=model
+        self.year=year
+        self.price=price
+        self.engin_type=engin_type
