e6975babdb7a6d4d5d4974045c4c764d8928cfcf
Dave <okanlawondavvv@gmail.com>
2024-08-24 13:29:08 +0100

    first commit

---
+++ basic.py
@@ -0,0 +1,59 @@
+# print("welcome to collaboration class")
+
+# def square(x):
+#     return x **5
+# print(square(4))
+
+
+
+"""
+working on object car
+types
+method: details(bukunmi)
+method: drive(pelumi)
+method: drift(boluwatife)
+method: park(ayodele)
+method: reverse(toluwanimi)
+method: payment(maimunat)
+method: landing page(Israel)
+method: ignition(david)
+method: neutral (Emmanuel)
+
+"""
+class Cars():
+    tyre=4
+    engine=1
+    bonnet=1
+    brake=1
+    accelerator=1
+    def __init__(self, payment, details, routine, landing_page, ignition, park, reverse, neutral, drive, drift):
+        self.payment=payment
+        self.details=details
+        self.routine=routine
+        self.landing_page=landing_page
+        self.ignition=ignition
+        self.park=park
+        self.reverse=reverse
+        self.neutral=neutral
+        self.drive=drive
+        self.drift=drift
+
+    
+
+
+
+# ayodele just join 
+
+
+# asgodwants just joined
+
+
+# Maimunat just joined..😊✌️
+
+# toluwanimi just joined
+
+# pelumi just joind
+
+# Israel just joined
+
+# dave just joined
