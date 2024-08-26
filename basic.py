06f8601825b76c02ab9daf6f0db6ade0c3f33402
Dave <okanlawondavvv@gmail.com>
2024-08-26 14:39:15 +0100

    changes made

--- basic.py
+++ basic.py
@@ -1,10 +1,8 @@
-# print("welcome to collaboration class")
+print("Welcome to collaboration class")
 
-# def square(x):
-#     return x **5
-# print(square(4))
-
-
+def square(x):
+    return x **5
+print(square(4))
 
 """
 working on object car
@@ -58,3 +56,29 @@ class Cars():
 
 # dave just joined
 # it's the end of the month
+
+import time
+
+class Cars:
+    def __init__(self):
+        pass
+    def ignition(self):
+        print("Insert the key into the ignition/Press the ignition buttton")
+        user = input('').capitalize().lower().strip()
+        if user == 'start':
+            print("Starting the engine...")
+            time.sleep(4) 
+            engine_status = "running..."
+            if engine_status == "running...":
+                print("The engine has started successfully!")
+            time.sleep(2)
+            print("The car is now ready to drive. Have a safe journey!")
+
+        else: 
+            print("Car failed to start!!!") 
+mycar = Cars()
+mycar.ignition()
+  
+
+
+
